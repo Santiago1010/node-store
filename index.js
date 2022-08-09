fbc1bcb274cae1250ada960e0f5bd56a67ad7b0d
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-08-09 09:36:28 -0500

    First routes

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.clear()
-console.log('Mi aplicación')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+	response.send('Hello! Wellcome to my server express.')
+})
+
+app.get('/products', (request, response) => {
+	response.json({
+		name: 'Product 1',
+		price: 1000
+	})
+})
+
+app.listen(port, () => {
+	console.log('Mi puerto: ' + port)
+})
